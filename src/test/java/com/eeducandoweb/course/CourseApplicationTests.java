00e8d98ffca5b329c2c8566a283f9dc57453f1ed
nelio alves <jessfreitas70@gmail.com>
2024-06-19 01:17:47 -0300

    projeto Spring Boot

---
+++ src/test/java/com/eeducandoweb/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eeducandoweb.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
